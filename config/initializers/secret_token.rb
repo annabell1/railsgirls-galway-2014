7c60a01942237ce798c96fa82d716431c9596913
annabell o'boyle <annabell053@hotmail.com>
2014-06-21 12:44:53 +0100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RailsgirlsGalwway2014::Application.config.secret_key_base = '28bcdb7b2bf438c678edd31f9f7796648db42542981b34e714c234f9e2123e38ddd09e5337afee9fe0e180b97b257b3ec753bc55a5123e6041637a9de4857ea9'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+RailsgirlsGalwway2014::Application.config.session_store :cookie_store, key: '_railsgirls-galwway-2014_session'
